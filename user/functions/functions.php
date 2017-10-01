df2643e4386f882e929cd09b3ad53f980fcf5bc1
Ari Bambang Kurniawan <aribk210@gmail.com>
2017-10-01 14:29:38 +0700

    V1.0
    Modif user css and my_account.php

---
+++ user/functions/functions.php
@@ -0,0 +1,21 @@
+<?php
+$db = mysqli_connect("localhost", "root","","Tugas-Besar");
+
+function login(){
+
+	if (isset($_POST['login'])){
+
+		global $db;
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$sql = "select * from User where username='$username' AND password='$password'";
+		$run_sql=mysqli_query($db,$sql);
+		$cek_user= mysqli_num_rows($run_sql);
+		if ($cek_user==0) {
+			echo "<script> alert('Username atau password SALAH') </script>";
+		} else {
+			echo "<script> window.open('my_account.php','_self') </script>";
+		}
+
+	}
+}

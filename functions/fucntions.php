4693d57937e901c5e68288987e9bd5137b60c133
Andrian Maulana <andrianmaulana28@gmail.com>
2017-10-02 08:38:02 +0700

    V1.0 test 1st commit untuk CRUD server side

--- functions/fucntions.php
+++ functions/fucntions.php
@@ -1,1 +1,31 @@
-<?php  ?>
+<?php
+$db = mysqli_connect("localhost", "root","","Tugas-Besar");
+
+function login(){
+
+  if (isset($_POST['login'])){
+
+    global $db;
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $sql = "select * from User where username='$username' AND password='$password'";
+    $run_sql=mysqli_query($db,$sql);
+    $cek_user= mysqli_num_rows($run_sql);
+    if ($cek_user==0) {
+      echo "<script> alert('Username atau password SALAH') </script>";
+    } else {
+      echo "<script> window.open('my_account.php','_self') </script>";
+    }
+
+  }
+}
+
+function account_register() {
+
+}
+function add(){
+  $this->load->view('register');
+}
+function add_account(){
+  
+}

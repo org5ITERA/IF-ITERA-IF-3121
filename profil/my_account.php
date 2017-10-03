89a9d6feefb54e502cbf0b8df98a4f0b8ed2a6fa
Ari Bambang Kurniawan <aribk210@gmail.com>
2017-10-04 03:47:01 +0700

    V1.0
    fix register.php
    add session on login and main

--- profil/my_account.php
+++ profil/my_account.php
@@ -3,6 +3,14 @@
 session_start();
 include("functions/functions.php");
 
+
+if (!isset($_SESSION['username'])) {
+  echo "<script>window.open('index.php','_self')</script>";
+}
+else {
+  echo "";
+}
+
 ?>
 <!DOCTYPE html>
 <head><title> Questions & Answer </title>
@@ -21,7 +29,26 @@ include("functions/functions.php");
         Akun Saya:
       </div>
       <ul id="sb">
-        <p style='color:#FFF'>Nama: <b>$c_name</b><br>Last Login:<br> $mysqldate </p>
+				<?php
+				$username = $_SESSION['username'];
+
+				$get_info = "select * from user where username='$username'";
+
+				$run_info = mysqli_query($db, $get_info);
+
+				$row_info = mysqli_fetch_array($run_info);
+
+
+				$nama = $row_info['nama'];
+        $lastlogin = $row_info['lastlogin'];
+
+        $phpdate = strtotime( $lastlogin );
+        $mysqldate = date( 'd-m-Y H:i:s', $phpdate );
+				echo "<br><p style='text-align:center;color:#FFF'>Nama: <b>$nama</b><br>Last Login:<br> $mysqldate </p>";
+
+
+				?>
+
         <a href="#">Pertanyaanku</a> <br>
         <a href="#">Jawabanku</a> <br>
         <a href="#">Pengaturan Akun </a><br>
